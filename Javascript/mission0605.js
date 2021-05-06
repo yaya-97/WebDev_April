8ad3aa1925e6ba9b441b73dc18c5d6b027dcc078
yaya-97 <yasminetay5@gmail.com>
2021-05-06 16:15:35 +0200

    CV intro done

---
+++ Javascript/mission0605.js
@@ -0,0 +1,22 @@
+CV = {
+    name: "Yasmine",
+    Fname: 'Tay',
+    age: 23,
+    status:'student',
+    "Is a student" : true,
+    city: 'Brussels',
+  
+
+    'Has finished studying Graphic Design': true,
+    'Has finished studying Webdesign': false,
+
+    study1:'Graphic design',
+    study2:'Webdesign',
+
+  };
+
+  console.log(
+      'Hello, my name is ' + CV.name + " " + CV.Fname + " and I'm " + CV.age + " years old. \nI currently live in "+CV.city+
+      " and am studying "+CV.study2+". \nLast year I finished a study in "+CV.study1+", and with \nextra knowledge in "+CV.study2+", i hope \nto soon find a nice job in those fields in "+CV.city+"."
+      
+  )
