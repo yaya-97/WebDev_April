731f1c4890edc794598598933ec3ff8d85c54959
yaya-97 <yasminetay5@gmail.com>
2021-05-07 11:33:37 +0200

    fixed everything

--- Javascript/m_0705.js
+++ Javascript/m_0705.js
@@ -20,8 +20,15 @@ const pProfile ={
     printPersonInfo:function(){
         console.log('User profile:')
         console.log(`Full name:${pProfile.fullN}\nAge:${pProfile.age}\nCountry:${pProfile.country}\nLocation:${pProfile.city}\nE-mail:${pProfile.email}`)
+        return `Full name:${pProfile.fullN}\nAge:${pProfile.age}\nCountry:${pProfile.country}\nLocation:${pProfile.city}\nE-mail:${pProfile.email}`
     }
 }
+
+// function clickEvent(){
+//     console.log('hello click')
+//     console.log(pProfile.printPersonInfo())
+//     document.getElementById('PROFILE').innerHTML= pProfile.printPersonInfo()
+// }
 
 pProfile.printPersonInfo();
 
