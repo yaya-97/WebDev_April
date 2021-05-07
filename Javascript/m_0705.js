5bf251c958a27691f20f3b06a23c787a2f9393d1
yaya-97 <yasminetay5@gmail.com>
2021-05-07 11:10:32 +0200

    assignment function display (still issues with html display)

---
+++ Javascript/m_0705.js
@@ -0,0 +1,27 @@
+const Uprofile = {
+    IDtag:'yaya-97',
+    initials:'YT',
+}
+
+function getPeople(Uprofile){
+    console.log('User: ',Uprofile.IDtag)
+}
+
+let nullObject = {}
+
+getPeople(Uprofile)
+
+const pProfile ={
+    fullN:'Yasmine TAY',
+    age:23,
+    email:'Yasmine.Tay@student.intecbrussel.be',
+    country:'Belgium',
+    city:'Brussels',
+    printPersonInfo:function(){
+        console.log('User profile:')
+        console.log(`Full name:${pProfile.fullN}\nAge:${pProfile.age}\nCountry:${pProfile.country}\nLocation:${pProfile.city}\nE-mail:${pProfile.email}`)
+    }
+}
+
+pProfile.printPersonInfo();
+
