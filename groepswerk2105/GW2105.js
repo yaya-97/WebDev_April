f4627fb2b7d1cca976c57877e6e2b685a1f9a08e
yaya-97 <yasminetay5@gmail.com>
2021-05-21 14:36:38 +0200

    updated and hopefully correct

--- groepswerk2105/GW2105.js
+++ groepswerk2105/GW2105.js
@@ -1,6 +1,6 @@
 let User = [
   {
-    id: "user1",
+    id: 1,
     fullname: "Yasmine Tay",
     email: "yasminetay5@gmail.com",
     phone: "0478593744" ,
@@ -9,7 +9,7 @@ let User = [
   },
 
   {
-    id: "user2",
+    id: 2,
     fullname: "Roger Kassanda",
     email: "rkassanda@gmail.com",
     phone: "0486513744",
@@ -18,7 +18,7 @@ let User = [
   },
 
   {
-    id: "user3",
+    id: 3,
     fullname: "Kabbaj Said",
     email: "mksaid@gmail.com",
     phone: "0474898733",
@@ -27,7 +27,7 @@ let User = [
   },
 
   {
-    id: "user4",
+    id: 4,
     fullname: "John Doe",
     email: "jdoe4555@gmail.com",
     phone: "0477536944",
@@ -36,7 +36,7 @@ let User = [
   },
 
 {
-    id: "user5",
+    id: 5,
     fullname: "Mary Caroline",
     email: "mctv458@gmail.com",
     phone: "0478540907",
@@ -50,23 +50,11 @@ add 5 User
 Use only array methods and no regular loops
 */
 
-function addUser(){
-    const addUser = User.push(
-        {
-            id: "user6",
-            fullname: "Maria Lopez",
-            email: "malopez79@gmail.com",
-            phone: "0477458933" ,
-            username: "malo_79",
-            password: "H2585d5fg69GFGdt54",    
-        },
-        
-    );
-};
-addUser();
-console.log(User);
-
+function addUser(id, fullname, email, phone, username, password){
   // params: id,fullname, email, phone, username,password
+  User.push({id, fullname, email, phone, username, password});
+};
+console.log(User)
 
 function getUser() {
     console.log(User);
@@ -80,31 +68,37 @@ function getSortedUsersByName() {
         if(a.fullname > b.fullname) { return 1; }
         return 0;
     })
-
+    console.log(User);
   // getAllUser sorted list by names
   // google is your friend: "How to sort an array of objects by a property value?"
 }
 
-getSortedUsersByName();
-console.log(User);
-
-
-function getUserByName() {
+function getUserByName(fullname) {
   // You should get single user object by name
-  let nameFilter = User.filter((x) => x.fullname == "John Doe");
+  let nameFilter = User.filter((x) => x.fullname == fullname);
 
     console.log(nameFilter);
 
 };
-getUserByName();
 
-
-function deleteUserById() {
+function deleteUserById(id) {
   // delete single user object by id
-  let removeUser =  User.filter((x) => x.id !== "user6");
-User=removeUser;
+  var index = User.map(function(e) { return e.id; }).indexOf(id);
+  if (index !== -1) {
+    User.splice(index, 1);
+  }
 };
-deleteUserById();
-console.log(User);
+
 // CONSOLE RESULT HERE
 //
+
+addUser(6,"Atilla Balin","abalin@SpeechGrammarList.com", "0475898987","abalin","45btgr5b8ty4");
+console.log(User);
+
+getSortedUsersByName();
+
+getUserByName("Yasmine Tay");
+
+
+deleteUserById(5);
+console.log(User);
