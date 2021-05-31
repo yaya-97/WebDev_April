5f66a52497f51bf9256ad63672251f88f8fea219
yaya-97 <yasminetay5@gmail.com>
2021-05-31 10:58:46 +0200

    updated assignment

--- 3105/whilemission.js
+++ 3105/whilemission.js
@@ -19,10 +19,15 @@ let hours = (totaltime/3600) % 24;
 let minutes = (totaltime/60) %60;
 let seconds = totaltime % 60;
 
+var result = document.getElementById("countdown")
+
+
 let i =totaltime
 setInterval(function(){
     i--
     console.log(Math.floor(days), 'days',Math.floor(hours), 'hours', Math.floor(minutes), 'minutes', Math.floor(seconds), 'seconds')
+  
+   result.innerHTML=`${Math.floor(days)} days, ${Math.floor(hours)} hours, ${Math.floor(minutes)} minutes, ${Math.floor(seconds)} seconds!`
     days = i/3600/24;
     hours = (i/3600) % 24;
     minutes = (i/60) %60;
