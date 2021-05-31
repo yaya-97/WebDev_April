a21d20b1b2d8f113a368c9f7491b18747b3183f1
yaya-97 <yasminetay5@gmail.com>
2021-05-31 10:28:39 +0200

    finished assignment

---
+++ 3105/whilemission.js
@@ -0,0 +1,31 @@
+let current_date = new Date();
+let end_date = new Date(1622505600*1000);
+let totaltime = (end_date - current_date)/1000;
+/*
+days = totaltime/3600/24
+hours (totaltime/3600) % 24
+mins (totaltime/60) %60
+seconds totaltime % 60
+*/
+console.log(current_date);
+
+console.log(totaltime);
+
+console.log(end_date);
+
+
+let days = totaltime/3600/24;
+let hours = (totaltime/3600) % 24;
+let minutes = (totaltime/60) %60;
+let seconds = totaltime % 60;
+
+let i =totaltime
+setInterval(function(){
+    i--
+    console.log(Math.floor(days), 'days',Math.floor(hours), 'hours', Math.floor(minutes), 'minutes', Math.floor(seconds), 'seconds')
+    days = i/3600/24;
+    hours = (i/3600) % 24;
+    minutes = (i/60) %60;
+    seconds = i % 60;
+},1000)
+
